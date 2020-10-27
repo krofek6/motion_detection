72b2bb7463456a1e11551a109d1b6f8b657120fc
krofek6 <69760905+krofek6@users.noreply.github.com>
2020-10-27 12:02:40 +0200

    Update motion_detection_save.py

--- motion_detection_save.py
+++ motion_detection_save.py
@@ -1,31 +1,16 @@
+#Save video motion detection-
+#Like regular motion detection, but also save all the frames with movment to a video file.
+#Write the original time of the frames (the time of the original video),
+#at the right corner of the frames.
+
+#NOTE: this code will take longer to run an use a lot more memory (RAM) then the regular
+#motion detection, it recomended to run while not working on the computer and without other
+#memory consuming programs (like chrome, madia player, etc.) open at the same time.
 import cv2
 
 def motion_detection(video, writer, buffer, textCoord, font):
-    """Main function of the Video class.
 
-    This function can work in two different way:
-        
-    Motion detection- analyze the diffrence between each frame and the previous frame.
-    With this way the function detect movment and not change.
-    For example, if an animal will relatively stay still in the frame,
-    the function will not detect it.
-    In addition, the function look for at least one significant diffrence between the
-    frames, and try to decide if there's a movment, so if there one movment or several
-    movments (simultaneously) the function will return the same value.
-
-    Background change detection- analyze the diffrence between the given  backgound
-    frame and all the othere frames.
-    With this way the function detect change from the "clear" frame (the background frame)
-    and not movment.
-    For example, if an animal will relatively stay still or move in the frame, the
-    function will sense it as "movment".
-    In addition, the function look for at least one significant diffrence between the
-    frames, and try to decide if there's a change, so if there one diffrence or several
-    diffrence (simultaneously) from the background frame, the function will return the
-    same value.
-    """
-##    font = cv2.FONT_HERSHEY_SIMPLEX
-    
+    #Create a list for saving the frames
     frameList = []
     
     #Set the the movment counter and movment flag
@@ -41,13 +26,14 @@ def motion_detection(video, writer, buffer, textCoord, font):
     #+ NOTE: frameCount usually doesn't get the correct number of frames! +
     for i in range(1,int(video.frameCount-2)):
 
-        #Read the next frame and edit it
+        #Read the next frame
         ret, frame = video.readVideo()
 
         # Check if the frame exists, if not break the loop because video end
         if not ret:
             break
-            
+
+        #Edit the next frame and set it as newFrame.
         resizeFrame, newFrame = video.edit_frame(frame)
             
         #Analyze the difference between the frames 
@@ -59,10 +45,9 @@ def motion_detection(video, writer, buffer, textCoord, font):
         for c in contours:
 
             #Check if the difference big enough
-
             if cv2.contourArea(c) > video.setting.MIN_DIFF:
 
-                #If it is, reste the no movment counter
+                #If it is, reset the no movment counter
                 noMovmentCounter = 0
 
                 # 0 = cv2.CAP_PROP_POS_MSEC
@@ -76,28 +61,32 @@ def motion_detection(video, writer, buffer, textCoord, font):
 
                     #If not, declare movment and calculate the movment time in seconds
                     movment = True
-
-
                     startTime = timer
 
-
-                    #Write previews frame
+                    #Write previews frames
                     for f in frameList:
+
+                        #Get the frame original time
                         timeText = video.second_to_timer(f[0]/1000)
+
+                        #Write the original time on the frame. 
                         cv2.putText(f[1],
                                     f'{timeText} {video.fileName}',
                                     textCoord,
                                     font,
                                     1,(255, 255, 255), 2,4)
+
                         
                         writer.write(f[1])
+
+                    #Reset the frames list
                     frameList = []
 
+                #Write the corrent frame with it original time
                 cv2.putText(frame,
                             f'{timer} {video.fileName}',
                             textCoord,
                             font, 1,(255, 255, 255), 2,4)
-##                cv2.putText(frame, timer,(50, 50), font, 1,(255, 0, 0),  2,4)
                 writer.write(frame)
 
                 #Break the loop (one difference is enough to declare movment
@@ -107,13 +96,15 @@ def motion_detection(video, writer, buffer, textCoord, font):
 
             #If the loop end without breaking, check if movment
             if movment:
-                
+
+                #Caculate the frame time  
                 # 0 = cv2.CAP_PROP_POS_MSEC
                 sec = video.video.get(0)/1000
 
                 #Convert the time from seconds to string and save it
                 timer = video.second_to_timer(sec)
 
+                #Write the corrent frame with it original time
                 cv2.putText(frame, f'{timer} {video.fileName}',textCoord, font, 1,(255, 255, 255), 2,4)
                 
                 writer.write(frame)
@@ -138,6 +129,7 @@ def motion_detection(video, writer, buffer, textCoord, font):
                     #Write the start and the stop time of the movment
                     video.setting.writeCSV(f'{video.fileName},{startTime},{stopTime}\n')
             else:
+                #Save the frame in a list as a "buffer" to write to the file
                 if len(frameList) > buffer:
                     frameList.pop(0)
                 frameList.append([video.video.get(0), frame])
@@ -150,6 +142,4 @@ def motion_detection(video, writer, buffer, textCoord, font):
 
     #End the video capture
     video.end_video_capture()
-
-##    writer.release()
 

0a9f1d0baf78b6cabc2eb351622901df44b8965c
krofek6 <69760905+krofek6@users.noreply.github.com>
2020-11-11 16:28:38 +0200

    Update motion_detection_save.py

--- motion_detection_save.py
+++ motion_detection_save.py
@@ -24,7 +24,7 @@ def motion_detection(video, writer, buffer, textCoord, font):
 
     #Start a for loop that run according  to the number of frames in the video.
     #+ NOTE: frameCount usually doesn't get the correct number of frames! +
-    for i in range(1,int(video.frameCount-2)):
+    for i in range(1,int(video.frameCount+10)):
 
         #Read the next frame
         ret, frame = video.readVideo()
